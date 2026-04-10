6bd1b3237310b1afa26b64c0868205bbe748cc85
Woodrow Crawford <woodrowcrawford03@gmail.com>
2026-04-10 19:34:05 -0400

    Updated the inventory ui

--- Assets/Scripts/Gameplay/InventorySlot.cs
+++ Assets/Scripts/Gameplay/InventorySlot.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -10,6 +11,8 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     //The current held item in this slot.
     [SerializeField] private GameObject currentItem;
 
+    [SerializeField] private TMP_Text itemNameText;
+
     //Public getter for the current item in this slot, and a boolean to check if the slot is empty.
     public GameObject CurrentItem => currentItem;
     public bool IsEmpty => currentItem == null;
@@ -20,6 +23,8 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     {
         //On awake, refresh the current item in the slot.
         RefreshCurrentItem();
+        itemNameText = GetComponentInChildren<TMP_Text>();
+        
     }
     
 
@@ -36,12 +41,17 @@ public class InventorySlot : MonoBehaviour, IDropHandler
         //set the current item to null
         currentItem = null;
 
+        //set the item name text to be empty
+        itemNameText.text = string.Empty;
+
         //in each item slot in the inventory, check if there is an item in the slot, and if there is, set the current item to that item
         foreach (Transform child in transform)
         {
             if (child.GetComponent<ItemBehavior>() != null)
             {
+                //set the current item and name to the slot
                 currentItem = child.gameObject;
+                itemNameText.text = currentItem.GetComponent<ItemBehavior>().ItemName;
                 break;
             }
         }
@@ -55,17 +65,39 @@ public class InventorySlot : MonoBehaviour, IDropHandler
 
         if (currentItem == null)
         {
+            itemNameText.text = string.Empty;
             return;
         }
 
         currentItem.transform.SetParent(transform, false);
-        currentItem.transform.localPosition = Vector3.zero;
-        currentItem.transform.localScale = new Vector3(1f, 1f, 1f);
+        SnapItemToSlot(currentItem);
 
         ItemBehavior itemBehavior = currentItem.GetComponent<ItemBehavior>();
         if (itemBehavior != null)
         {
             itemBehavior.SetCurrentSlot(this);
+            itemNameText.text = itemBehavior.ItemName;
+        }
+    }
+
+    //A function that snaps the given item to the center of the slot, and resets its rotation and scale.
+    private void SnapItemToSlot(GameObject item)
+    {
+        RectTransform itemRect = item.GetComponent<RectTransform>();
+
+        if (itemRect != null)
+        {
+            // Snap the item to the center of the slot
+            itemRect.anchoredPosition = Vector2.zero;
+            itemRect.localRotation = Quaternion.identity;
+            itemRect.localScale = Vector3.one;
+        }
+        else
+        {
+            // If the item doesn't have a RectTransform, just reset its local position, rotation, and scale
+            item.transform.localPosition = Vector3.zero;
+            item.transform.localRotation = Quaternion.identity;
+            item.transform.localScale = Vector3.one;
         }
     }
 
@@ -78,39 +110,59 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     //This function is called when an item is dropped after being dragged over a slot.
     public void OnDrop(PointerEventData eventData)
     {
+        //Get the dropped item from the event data, and check if it has an ItemBehavior component.
         GameObject droppedItem = eventData.pointerDrag;
         if (droppedItem == null)
         {
             return;
         }
 
+        //get the item behavior from the dropped item, and if it doesnt have one then return
         ItemBehavior droppedItemBehavior = droppedItem.GetComponent<ItemBehavior>();
         if (droppedItemBehavior == null)
         {
             return;
         }
 
+
+        //set the previous slot of the dropped item to this slot, and get the previous item in this slot
         InventorySlot previousSlot = droppedItemBehavior.CurrentSlot;
         GameObject previousItem = currentItem;
 
+        //if this is the same slot that the item was dragged from, then we just need to refresh the current item in this slot and return
         if (previousSlot == this)
         {
             SetCurrentItem(droppedItem);
             return;
         }
 
-        SetCurrentItem(droppedItem);
-
+        //if the previous slot is not null, then we need to clear the current item in the previous slot to prepare for the item being moved to this slot
         if (previousSlot != null)
         {
-            if (previousItem != null && previousItem != droppedItem)
-            {
-                previousSlot.SetCurrentItem(previousItem);
-            }
-            else
-            {
-                previousSlot.ClearCurrentItem();
-            }
+            previousSlot.ClearCurrentItem();
         }
+
+        //clear the current item in this slot to prepare for the new item being dropped here
+        ClearCurrentItem();
+
+        //if the previous slot was not null and the previous item is not null and the previous item is not the same as the dropped item...
+        if (previousSlot != null && previousItem != null && previousItem != droppedItem)
+        {
+            //set the current item in the previous slot to the previous item
+            previousSlot.SetCurrentItem(previousItem);
+        }
+
+        //set the current item in this slot to the dropped item
+        SetCurrentItem(droppedItem);
+
+        //if the previous slot was not null...
+        if (previousSlot != null)
+        {
+            //refresh the previous slot to update its current item and name text
+            previousSlot.RefreshCurrentItem();
+        }
+
+        //refresh this slot to update its current item and name text
+        RefreshCurrentItem();
     }
 }
